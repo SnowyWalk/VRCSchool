4c97cfeb3e747db387c44e44a9ede53212c9d846
SnowyWalk <chunsa1166jp@gmail.com>
2026-01-31 01:49:24 +0900

    이쁘게 꾸미기

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,11 +1,18 @@
 import { source } from '@/lib/source';
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
+import { DocsLayout } from '@/components/layout/notebook';
 import { baseOptions } from '@/lib/layout.shared';
 
-export default function Layout({ children }: LayoutProps<'/docs'>) {
-  return (
-    <DocsLayout tree={source.getPageTree()} {...baseOptions()}>
-      {children}
-    </DocsLayout>
-  );
+export default function Layout({children}: LayoutProps<'/docs'>) {
+    const {nav, ...base} = baseOptions();
+
+    return (
+        <DocsLayout
+            {...base}
+            nav={{...nav, mode: 'top'}}
+            tabMode={"sidebar"}
+            tree={source.getPageTree()}
+        >
+            {children}
+        </DocsLayout>
+    );
 }
