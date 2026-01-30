2796e739dcbe5268de1318a635424fb991ebfe12
SnowyWalk <chunsa1166jp@gmail.com>
2026-01-30 23:22:28 +0900

    클플, 깃허브액션 초기 세팅

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,21 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import {createMDX} from 'fumadocs-mdx/next';
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
-  reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/docs/:path*.mdx',
-        destination: '/llms.mdx/docs/:path*',
-      },
-    ];
-  },
+    reactStrictMode: true,
+    async rewrites() {
+        return [
+            {
+                source: '/docs/:path*.mdx',
+                destination: '/llms.mdx/docs/:path*',
+            },
+        ];
+    },
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default withMDX(config);
